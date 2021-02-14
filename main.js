730a23787f760c8b44dead77b4b17873922c747d
=Rob Franke <robwfranke@gmail.com>
2021-02-14 19:33:18 +0100

    ready for dispatch

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 // const axios = require("axios");
-const searchButton = document.getElementById("searchButton")
-// console.log(searchButton) checking of het bestaat
-//
+const searchButton = document.getElementById("searchButton");
 
 
 async function getCountryInfo() {
@@ -11,71 +9,61 @@ async function getCountryInfo() {
     //------------------------------------------------------------------
 
     //resetten errormessage
-    const errorMessageToWeb = document.getElementById("errorMessageWeb")
-    errorMessageToWeb.textContent = ``
+    const errorMessageToWeb = document.getElementById("errorMessageWeb");
+    errorMessageToWeb.textContent = ``;
 
 
     // resetten flag en country
-    const flagOfCountryWeb = document.getElementById("flag")
-    flagOfCountryWeb.removeAttribute("src")
-    flagOfCountryWeb.removeAttribute("alt")
-    const flagCountryToWeb = document.getElementById("flagCountry")
-    flagCountryToWeb.textContent = ``
+    const flagOfCountryWeb = document.getElementById("flag");
+    flagOfCountryWeb.removeAttribute("src");
+    flagOfCountryWeb.removeAttribute("alt");
+    const flagCountryToWeb = document.getElementById("flagCountry");
+    flagCountryToWeb.textContent = ``;
 
     // resetten text
 
-    const countryNameStringtoWeb = document.getElementById("countryNameStringWeb")
-    countryNameStringtoWeb.textContent = ``
+    const countryNameStringtoWeb = document.getElementById("countryNameStringWeb");
+    countryNameStringtoWeb.textContent = ``;
 
-    const capitalStringToWeb = document.getElementById("capitalStringWeb")
+    const capitalStringToWeb = document.getElementById("capitalStringWeb");
 
-    capitalStringToWeb.textContent = ``
+    capitalStringToWeb.textContent = ``;
 
-    const stringOfLanguagesToWeb = document.getElementById("stringOfLanguagesWeb")
-    stringOfLanguagesToWeb.textContent = ``
+    const stringOfLanguagesToWeb = document.getElementById("stringOfLanguagesWeb");
+    stringOfLanguagesToWeb.textContent = ``;
     //------------------------------------------------------------------
 
 
     //---------------ophalen landnaam-------------
 
-    const country = document.getElementById("countryNameWeb").value
+    const country = document.getElementById("countryNameWeb").value;
     //------------------------------------------------------------------
 
 
     try {
-        const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true` /*LET OP BACKTICK!!!!!!!!!!!*/
-        // console.log("url=", url)
-        // console.log("get country here", country)
-        const response = await axios.get(url)
+        const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`; /*LET OP BACKTICK!!!!!!!!!!!*/
+        const response = await axios.get(url);
 
 
-        //---------------ophalen landnaam-------------
+        //-----------------------------
         // Input weghalen
-        //---------------ophalen landnaam-------------
+        //-------------------------------
 
-        document.getElementById('countryNameWeb').value = ""
+        document.getElementById('countryNameWeb').value = "";
 
 
-// opdracht 2, maak string  [country-naam]
-// is situated in [subarea-name]. It has a population of [amount] people.
+// opdracht 2, maak string  [countryNameString
 
-        console.log(response.data[0].name)/*array, met 1 object erin, deze bevat bijv name*/
-        console.log(response.data[0].subregion)/*array, met 1 object erin, deze bevat bijv subregion*/
-        console.log(response.data[0].population)/*array, met 1 object erin, deze bevat bijv subregion*/
         const countryName = response.data[0].name;
         const subareaName = response.data[0].subregion;
-        const amountPopulation = response.data[0].population
-        const countryNameString = countryName + "  is situated in " + subareaName + ". It has a population of " + amountPopulation + " people."
-
-        console.log("Opdracht 2= ", countryNameString)
+        const amountPopulation = response.data[0].population;
+        const countryNameString = countryName + "  is situated in " + subareaName + ". It has a population of " + amountPopulation + " people.";
 
 
 // opdracht 3
 //Maak op basis van de response de volgende string en log dit in de console: The capital is [city]
 
         let capitalString = "The capital is " + response.data[0].capital;
-
-        console.log("Opdracht 3:", capitalString)
 
 
         //Opdracht 4
@@ -85,26 +73,15 @@ async function getCountryInfo() {
         //     1 valuta: and you can pay with [currency]'s
         // 2 valuta's: and you can pay with [currency]'s and [currency]'s
 
-        const responseArrayLength = response.data[0].currencies.length
-        console.log("aantal currencies", responseArrayLength)
 
-
-        // const CurrencyArray = response.data[0].currencies[2].name
-        let listOfCurrencies = ""
+        let listOfCurrencies = "";
 
         for (let i = 0; i < response.data[0].currencies.length; i++) {
-
             if (response.data[0].currencies[i].name != null) {
-                // console.log(response.data[0].currencies[i].name)
-                listOfCurrencies = listOfCurrencies + response.data[0].currencies[i].name + ","
-                // console.log(listOfCurrencies)
+                listOfCurrencies = listOfCurrencies + response.data[0].currencies[i].name + ",";
             }
-
-
         }
-        listOfCurrencies = listOfCurrencies.slice(0, -1)/* verwijder laatste komma*/
-
-        console.log("Opdracht 4: lijst met valuta: ", listOfCurrencies)
+        listOfCurrencies = listOfCurrencies.slice(0, -1);/* verwijder laatste komma*/
 
 
         //  Opdracht 5:
@@ -118,65 +95,58 @@ async function getCountryInfo() {
         // 3 talen: They speak [language], [language] and [language]
         // etc.
 
-        const numberOfLanguages = response.data[0].languages.length
-
-        let stringOfLanguages = "They speak " + response.data[0].languages[0].name
-
+        const numberOfLanguages = response.data[0].languages.length;
+        let stringOfLanguages = "They speak " + response.data[0].languages[0].name;
         for (let i = 1; i < numberOfLanguages - 1; i++) {
-
-
             if (numberOfLanguages - i > 0) {
-                stringOfLanguages = stringOfLanguages + ", " + response.data[0].languages[i].name
+                stringOfLanguages = stringOfLanguages + ", " + response.data[0].languages[i].name;
             }
-
-
         }
         if (numberOfLanguages > 1) {
-
-            stringOfLanguages = stringOfLanguages + " and " + response.data[0].languages[numberOfLanguages - 1].nativeName
+            stringOfLanguages = stringOfLanguages + " and " + response.data[0].languages[numberOfLanguages - 1].name;
         }
-
-        console.log(stringOfLanguages)
 
 
         //---------------------------------------------------------------
         //--------------- hier goede vlag in webpage zetten--------------
-        const flagOfCountryWeb = document.getElementById("flag")
-        const flagOfCountry = response.data[0].flag
-        console.log("flagOfCountry", flagOfCountry)
-        flagOfCountryWeb.setAttribute("src", flagOfCountry)
-        flagOfCountryWeb.setAttribute("alt", "flag")
-        const flagCountryToWeb = document.getElementById("flagCountry")
+        //---------------------------------------------------------------
+
+        const flagOfCountryWeb = document.getElementById("flag");
+        const flagOfCountry = response.data[0].flag;
+        flagOfCountryWeb.setAttribute("src", flagOfCountry);
+        flagOfCountryWeb.setAttribute("alt", "flag");
+        const flagCountryToWeb = document.getElementById("flagCountry");
         flagCountryToWeb.textContent = `${country}`
         //---------------------------------------------------------------
 
+        //---------------------------------------------------------------
+        //--------------- hier goede text in webpage zetten--------------
+        //---------------------------------------------------------------
+        const countryNameStringtoWeb = document.getElementById("countryNameStringWeb");
+        countryNameStringtoWeb.textContent = `${countryNameString}`;
 
-        const countryNameStringtoWeb = document.getElementById("countryNameStringWeb")
-        countryNameStringtoWeb.textContent = `${countryNameString}`
+        const capitalStringToWeb = document.getElementById("capitalStringWeb");
+        capitalString = capitalString + " and you can pay with " + listOfCurrencies + "'s";
+        capitalStringToWeb.textContent = `${capitalString}`;
 
-        const capitalStringToWeb = document.getElementById("capitalStringWeb")
-        capitalString = capitalString + " and you can pay with " + listOfCurrencies + "'s"
-        capitalStringToWeb.textContent = `${capitalString}`
+        const stringOfLanguagesToWeb = document.getElementById("stringOfLanguagesWeb");
+        stringOfLanguagesToWeb.textContent = `${stringOfLanguages}`;
 
-        const stringOfLanguagesToWeb = document.getElementById("stringOfLanguagesWeb")
-        stringOfLanguagesToWeb.textContent = `${stringOfLanguages}`
+
+        //***************  END of TRY *****************************************************
+
+
+
+        //***********************************************************************************
+        //***********************************************************************************
+        //***********************************************************************************
 
 
     } catch (errorDescription) {
 
 
-        const errorMessageToWeb = document.getElementById("errorMessageWeb")
-        errorMessageToWeb.textContent = `Er is iets fout gegaan`
-
-        // console.log("error", errorDescription.message)
-        // console.log("error response", errorDescription.response)
-        // const messageError = "Er is iets fout gegaan"
-        // const errorElement = document.createElement("h1")
-        // errorElement.textContent = messageError
-        // const errorText = document.getElementById("errorMessage");
-        // errorText.appendChild(errorElement)
-
-
+        const errorMessageToWeb = document.getElementById("errorMessageWeb");
+        errorMessageToWeb.textContent = `Er is iets fout gegaan`;
     }
 
 
@@ -188,24 +158,12 @@ async function getCountryInfo() {
 // -------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------
 searchButton.addEventListener("click", getCountryInfo);
-// searchButton.addEventListener("click", testFunction); je kunt meerder async functions
-//op dezelfde searchbutton zetten
-//
-// function testFunction() {
-//
-//     console.log("testFunction")
-//
-//     const textje = document.getElementById("countryNameWeb").value
-//     console.log("textje ", textje)
-// }
+
 
 
 // Get the input field  WANNEER DE ENTER KEY GEDRUKT WORDT, IS DAT NET ALSOF JE DE SEARCH KNOP INDRUKT
 const input = document.getElementById("countryNameWeb");
-
-// Execute a function when the user releases a key on the keyboard
 input.addEventListener("keyup", function (event) {
-    // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
         // Cancel the default action, if needed
         event.preventDefault();
